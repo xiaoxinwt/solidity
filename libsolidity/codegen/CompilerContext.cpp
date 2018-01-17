0189bc597fc5e0bbee5e4ab73606538da13a48b7
chriseth <chris@ethereum.org>
2018-01-17 12:05:43 +0100

    Prevent externally used functions from being removed.

--- libsolidity/codegen/CompilerContext.cpp
+++ libsolidity/codegen/CompilerContext.cpp
@@ -285,6 +285,7 @@ void CompilerContext::resetVisitedNodes(ASTNode const* _node)
 void CompilerContext::appendInlineAssembly(
 	string const& _assembly,
 	vector<string> const& _localVariables,
+	set<string> const& _externallyUsedFunctions,
 	bool _system
 )
 {
@@ -364,7 +365,7 @@ void CompilerContext::appendInlineAssembly(
 	}
 
 	julia::OptimiserSuite suite;
-	suite.run(*parserResult, analysisInfo);
+	suite.run(*parserResult, analysisInfo, _externallyUsedFunctions);
 
 	analysisInfo = assembly::AsmAnalysisInfo{};
 	analyzerResult = assembly::AsmAnalyzer(
